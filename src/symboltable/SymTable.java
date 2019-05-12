92b78bec6d1c980d340c53e173a0ea4e8c1e1405
czy <1600012904@pku.edu.cn>
2019-05-12 15:55:56 +0800

    之前的作业全部移到minijava文件夹

--- src/symboltable/SymTable.java
+++
@@ -1,8 +0,0 @@
-package symboltable;
-
-import java.util.Hashtable;
-
-public class SymTable {
-	//哈希表classTable的每项，储存一个类的符号信息
-	public Hashtable<String, ClassSymTable> classTable= new Hashtable<String, ClassSymTable>();
-}
